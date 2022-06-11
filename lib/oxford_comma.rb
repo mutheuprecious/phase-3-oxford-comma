28adc667f8eef737860f2d4eb10f1b588254b76c
Precious <preciousmaregwa1997@gmail.com>
2022-06-11 06:41:23 +0300

    complete assigment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+      array[0]
+    elsif array.length == 2
+      array.join(" and ")
+    elsif array.length > 2
+    new_array = array.insert(-2, "and")
+    new_array.slice(0..-3).join(", ") + (", ") + new_array.slice(-2,2).join(" ")
+    end
+  end
